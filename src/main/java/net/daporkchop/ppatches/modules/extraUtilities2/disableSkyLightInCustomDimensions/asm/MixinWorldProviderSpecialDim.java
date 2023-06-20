2728460395f2606de24d4280387f6b651621225f
DaPorkchop_ <daporkchop@daporkchop.net>
2023-06-20 17:48:34 +0200

    extraUtilities2.disableSkyLightInCustomDimensions: avoid setting chunk sky light arrays in quantum quarry dimension

--- src/main/java/net/daporkchop/ppatches/modules/extraUtilities2/disableSkyLightInCustomDimensions/asm/MixinWorldProviderSpecialDim.java
+++ src/main/java/net/daporkchop/ppatches/modules/extraUtilities2/disableSkyLightInCustomDimensions/asm/MixinWorldProviderSpecialDim.java
@@ -1,12 +1,15 @@
 package net.daporkchop.ppatches.modules.extraUtilities2.disableSkyLightInCustomDimensions.asm;
 
 import net.daporkchop.ppatches.PPatchesMod;
+import net.minecraft.world.chunk.NibbleArray;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
@@ -38,5 +41,14 @@ abstract class MixinWorldProviderSpecialDim extends MixinXUWorldProvider {
         PPatchesMod.LOGGER.info("extraUtilities2.disableSkyLightInCustomDimensions: disabling sky light in dimension {}: \"{}\"",
                 this.getDimensionType().getId(), this.getDimensionType().getName());
         this.hasSkyLight = false;
+    }
+
+    @Dynamic
+    @Redirect(method = "Lcom/rwtema/extrautils2/dimensions/workhousedim/WorldProviderSpecialDim;generate(IILnet/minecraft/world/gen/ChunkProviderServer;Ljava/lang/Object;)V",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/world/chunk/storage/ExtendedBlockStorage;setSkyLight(Lnet/minecraft/world/chunk/NibbleArray;)V", remap = true),
+            allow = 2, require = 2)
+    private static void ppatches_disableSkyLightInCustomDimensions_generate_dontSetSkyLightArray(ExtendedBlockStorage storage, NibbleArray array) {
+        //do nothing, we don't want to set the chunk section's sky light array since the chunk has no sky light
     }
 }
