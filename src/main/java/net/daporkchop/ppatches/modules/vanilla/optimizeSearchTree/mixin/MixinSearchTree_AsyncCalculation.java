e5c22658480d3819ced557533d4457897fdbc6ff
DaPorkchop_ <daporkchop@daporkchop.net>
2024-01-13 17:44:13 +0100

    vanilla.optimizeSearchTree: correctly initialize future tasks when recalculating search tree from server thread

--- src/main/java/net/daporkchop/ppatches/modules/vanilla/optimizeSearchTree/mixin/MixinSearchTree_AsyncCalculation.java
+++ src/main/java/net/daporkchop/ppatches/modules/vanilla/optimizeSearchTree/mixin/MixinSearchTree_AsyncCalculation.java
@@ -1,7 +1,7 @@
 package net.daporkchop.ppatches.modules.vanilla.optimizeSearchTree.mixin;
 
+import lombok.SneakyThrows;
 import net.daporkchop.ppatches.PPatchesMod;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.util.SearchTree;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,6 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.ForkJoinWorkerThread;
 
 /**
  * @author DaPorkchop_
@@ -30,11 +31,12 @@ abstract class MixinSearchTree_AsyncCalculation<T> {
             at = @At("HEAD"),
             cancellable = true,
             allow = 1, require = 1)
-    private void ppatches_optimizeSearchTree_recalculate_beginAsyncCalculation(CallbackInfo ci) {
-        if (Minecraft.getMinecraft().isCallingFromMinecraftThread()) {
-            if (this.ppatches_optimizeSearchTree_recalculateTask != null) { //wait for an existing task to complete, if any
+    @SneakyThrows(InterruptedException.class)
+    private synchronized void ppatches_optimizeSearchTree_recalculate_beginAsyncCalculation(CallbackInfo ci) {
+        if (!(Thread.currentThread() instanceof ForkJoinWorkerThread)) {
+            while (this.ppatches_optimizeSearchTree_recalculateTask != null && !this.ppatches_optimizeSearchTree_recalculateTask.isDone()) { //wait for an existing task to complete, if any
                 PPatchesMod.LOGGER.trace("SearchTree {} already has an async calculation task, waiting for it to complete...", this);
-                this.ppatches_optimizeSearchTree_recalculateTask.join();
+                this.wait();
             }
 
             PPatchesMod.LOGGER.trace("Creating async SearchTree calculation task for {}", this);
@@ -48,8 +50,9 @@ abstract class MixinSearchTree_AsyncCalculation<T> {
     @Inject(method = "Lnet/minecraft/client/util/SearchTree;recalculate()V",
             at = @At("RETURN"),
             allow = 1, require = 1)
-    private void ppatches_optimizeSearchTree_recalculate_notifyAsyncCalculationComplete(CallbackInfo ci) {
+    private synchronized void ppatches_optimizeSearchTree_recalculate_notifyAsyncCalculationComplete(CallbackInfo ci) {
         PPatchesMod.LOGGER.trace("Asynchronous SearchTree calculation for {} complete", this);
+        this.notifyAll();
     }
 
     @Inject(method = "Lnet/minecraft/client/util/SearchTree;search(Ljava/lang/String;)Ljava/util/List;",
