2283b4560cc7542c6077ad758283f3ad89267718
DaPorkchop_ <daporkchop@daporkchop.net>
2024-02-04 17:51:45 +0100

    forge.optimizeGetPersistentChunks: make compatible with cubic chunks
    this also cleans up the implementation pretty substantially, now that i can make existing fields final at will :)

--- src/main/java/net/daporkchop/ppatches/modules/forge/optimizeGetPersistentChunks/mixin/MixinForgeChunkManager.java
+++ src/main/java/net/daporkchop/ppatches/modules/forge/optimizeGetPersistentChunks/mixin/MixinForgeChunkManager.java
@@ -2,146 +2,58 @@ package net.daporkchop.ppatches.modules.forge.optimizeGetPersistentChunks.mixin;
 
 import com.google.common.collect.ImmutableSetMultimap;
 import net.daporkchop.ppatches.modules.forge.optimizeGetPersistentChunks.util.IMixinWorld_OptimizeGetPersistentChunks;
+import net.daporkchop.ppatches.util.mixin.ext.MakeFinal;
 import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeChunkManager;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
 
+import java.util.AbstractMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author DaPorkchop_
  */
-@SuppressWarnings("unchecked")
 @Mixin(value = ForgeChunkManager.class, remap = false)
 abstract class MixinForgeChunkManager {
-    /*@Redirect(method = "<clinit>",
-            at = @At(value = "INVOKE",
-                    target = "Lcom/google/common/collect/MapMaker;makeMap()Ljava/util/concurrent/ConcurrentMap;"),
-            allow = 2, require = 2)
-    private static ConcurrentMap<?, ?> ppatches_optimizeGetPersistentChunks_$clinit$_initializeTicketsAndDormantChunkCacheMapsToNull(MapMaker mapMaker) {
-        return null;
-    }*/
+    //we want this field to be final to help optimization
+    @MakeFinal
+    @Shadow
+    private static Map<World, ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket>> forcedChunks;
 
     @Redirect(method = "<clinit>",
             at = @At(value = "INVOKE",
                     target = "Ljava/util/Collections;synchronizedMap(Ljava/util/Map;)Ljava/util/Map;"),
             allow = 1, require = 1)
-    private static Map<?, ?> ppatches_optimizeGetPersistentChunks_$clinit$_initializeForcedChunksMapToNull(Map<?, ?> map) {
-        return null;
-    }
+    private static Map<World, ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket>> ppatches_optimizeGetPersistentChunks_$clinit$_initializeForcedChunksMapToRedirectingVersion(Map<?, ?> map) {
+        return new AbstractMap<World, ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket>>() {
+            @Override
+            public Set<Entry<World, ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket>>> entrySet() {
+                throw new UnsupportedOperationException();
+            }
 
-    /*@Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;loadWorld(Lnet/minecraft/world/World;)V",
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
-                    ordinal = 0),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_loadWorld_storeTickets(Map<?, ?> map, Object world, Object newTickets) {
-        ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_tickets((Multimap<String, ForgeChunkManager.Ticket>) newTickets);
-        return null;
-    }*/
+            @Override
+            public ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket> get(Object key) {
+                return ((IMixinWorld_OptimizeGetPersistentChunks) key).ppatches_optimizeGetPersistentChunks_forcedChunks();
+            }
 
-    @Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;loadWorld(Lnet/minecraft/world/World;)V",
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
-                    ordinal = 1),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_loadWorld_storeForcedChunks(Map<?, ?> map, Object world, Object newForcedChunks) {
-        ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_forcedChunks((ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket>) newForcedChunks);
-        return null;
-    }
+            @Override
+            public ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket> put(World key, ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket> value) {
+                ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket> old = ((IMixinWorld_OptimizeGetPersistentChunks) key).ppatches_optimizeGetPersistentChunks_forcedChunks();
+                ((IMixinWorld_OptimizeGetPersistentChunks) key).ppatches_optimizeGetPersistentChunks_forcedChunks(value);
+                return old;
+            }
 
-    /*@Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;loadWorld(Lnet/minecraft/world/World;)V",
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;",
-                    ordinal = 2),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_loadWorld_storeDormantChunkCache(Map<?, ?> map, Object world, Object newDormantChunkCache) {
-        ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_dormantChunkCache((Cache<Long, ?>) newDormantChunkCache);
-        return null;
-    }*/
-
-    /*@Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;loadWorld(Lnet/minecraft/world/World;)V",
-            slice = @Slice(from = @At(value = "FIELD",
-                    target = "Lnet/minecraftforge/common/ForgeChunkManager;tickets:Ljava/util/Map;",
-                    opcode = Opcodes.GETSTATIC,
-                    ordinal = 1)),
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;",
-                    ordinal = 0),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_loadWorld_redirectTicketsAccessToWorld(Map<?, ?> map, Object world) {
-
-        return null;
-    }*/
-
-    @Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;unloadWorld(Lnet/minecraft/world/World;)V",
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;remove(Ljava/lang/Object;)Ljava/lang/Object;",
-                    ordinal = 0),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_unloadWorld_clearForcedChunks(Map<?, ?> map, Object world) {
-        ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_forcedChunks(null);
-        return null;
-    }
-
-    @Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;forceChunk(Lnet/minecraftforge/common/ForgeChunkManager$Ticket;Lnet/minecraft/util/math/ChunkPos;)V",
-            slice = @Slice(from = @At(value = "FIELD",
-                    target = "Lnet/minecraftforge/common/ForgeChunkManager;forcedChunks:Ljava/util/Map;",
-                    opcode = Opcodes.GETSTATIC)),
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_forceChunk_getForcedChunks(Map<?, ?> map, Object world) {
-        return ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_forcedChunks();
-    }
-
-    @Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;forceChunk(Lnet/minecraftforge/common/ForgeChunkManager$Ticket;Lnet/minecraft/util/math/ChunkPos;)V",
-            slice = @Slice(from = @At(value = "FIELD",
-                    target = "Lnet/minecraftforge/common/ForgeChunkManager;forcedChunks:Ljava/util/Map;",
-                    opcode = Opcodes.GETSTATIC)),
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_forceChunk_storeForcedChunks(Map<?, ?> map, Object world, Object newForcedChunks) {
-        ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_forcedChunks((ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket>) newForcedChunks);
-        return null;
-    }
-
-    @Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;unforceChunk(Lnet/minecraftforge/common/ForgeChunkManager$Ticket;Lnet/minecraft/util/math/ChunkPos;)V",
-            slice = @Slice(from = @At(value = "FIELD",
-                    target = "Lnet/minecraftforge/common/ForgeChunkManager;forcedChunks:Ljava/util/Map;",
-                    opcode = Opcodes.GETSTATIC)),
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_unforceChunk_getForcedChunks(Map<?, ?> map, Object world) {
-        return ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_forcedChunks();
-    }
-
-    @Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;unforceChunk(Lnet/minecraftforge/common/ForgeChunkManager$Ticket;Lnet/minecraft/util/math/ChunkPos;)V",
-            slice = @Slice(from = @At(value = "FIELD",
-                    target = "Lnet/minecraftforge/common/ForgeChunkManager;forcedChunks:Ljava/util/Map;",
-                    opcode = Opcodes.GETSTATIC)),
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;put(Ljava/lang/Object;Ljava/lang/Object;)Ljava/lang/Object;"),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_unforceChunk_storeForcedChunks(Map<?, ?> map, Object world, Object newForcedChunks) {
-        ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_forcedChunks((ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket>) newForcedChunks);
-        return null;
-    }
-
-    @Redirect(method = "Lnet/minecraftforge/common/ForgeChunkManager;getPersistentChunksFor(Lnet/minecraft/world/World;)Lcom/google/common/collect/ImmutableSetMultimap;",
-            slice = @Slice(from = @At(value = "FIELD",
-                    target = "Lnet/minecraftforge/common/ForgeChunkManager;forcedChunks:Ljava/util/Map;",
-                    opcode = Opcodes.GETSTATIC)),
-            at = @At(value = "INVOKE",
-                    target = "Ljava/util/Map;get(Ljava/lang/Object;)Ljava/lang/Object;"),
-            allow = 1, require = 1)
-    private static Object ppatches_optimizeGetPersistentChunks_getPersistentChunksFor_getForcedChunks(Map<?, ?> map, Object world) {
-        return ((IMixinWorld_OptimizeGetPersistentChunks) world).ppatches_optimizeGetPersistentChunks_forcedChunks();
+            @Override
+            public ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket> remove(Object key) {
+                ImmutableSetMultimap<ChunkPos, ForgeChunkManager.Ticket> old = ((IMixinWorld_OptimizeGetPersistentChunks) key).ppatches_optimizeGetPersistentChunks_forcedChunks();
+                ((IMixinWorld_OptimizeGetPersistentChunks) key).ppatches_optimizeGetPersistentChunks_forcedChunks(null);
+                return old;
+            }
+        };
     }
 }
